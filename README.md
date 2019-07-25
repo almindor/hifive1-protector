e67baca1e95777bffb30a347ef9e025d561785c7
Ales Katona <almindor@gmail.com>
2019-07-25 11:26:12 -0600

    use DIG7 (GPIO 23) PIN shorting as trigger

--- README.md
+++ README.md
@@ -8,9 +8,7 @@ The protector (some call it "bootloader") code is meant to prevent getting the h
 
 ## Usage
 
-This crate overrides the "bootloader" memory at `0x20000000` and writes the current version of the protector code. The code as of this version just waits for 10s blinking the blue on-board LED before jumping to the user program code address for the given board. This allows restoring a program in the span of the wait period should the user program invalidate the CPU for uploading.
-
-Future versions will probably mimic the original behavior via reset button state switching.
+This crate overrides the "bootloader" memory at `0x20000000` and writes the current version of the protector code. The protector normally jumps to user code address specific to each board. If you need to "stop" on boot in order to flash a different user program short digital pin 7 (GPIO 23) to any ground (GND) and press the reset button. The protector will indicate being in "flash mode" by blinking the blue LED in an endless loop.
 
 ### Hifive1
 
--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,26 @@ fn main() -> ! {
     // Configure clocks
     let clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
     
-    // get LED pins
-    let mut blue_led = pin!(pins, led_blue).into_inverted_output();
+    // get "go to flash mode" DIG7 (GPIO pin 23)
+    let flash_mode_pin = pin!(pins, dig7).into_pull_up_input();
 
-    let mut sleep = Sleep::new(dr.core_peripherals.clint.mtimecmp, clocks);
+    // if we shorted DIG7 (GPIO pin 23) to GND we want to go to "flash mode"
+    if flash_mode_pin.is_low().unwrap() {
+        // get blue LED pin
+        let mut blue_led = pin!(pins, led_blue).into_inverted_output();
+        // set up sleeping
+        let mut sleep = Sleep::new(dr.core_peripherals.clint.mtimecmp, clocks);
 
-    for _ in 0..9 {
-        blue_led.on();
-        sleep.delay_ms(500u32);
-        blue_led.off();
-        sleep.delay_ms(500u32);
+        // loop endlessly blinking blue led so program can be uploaded
+        loop {
+            blue_led.on();
+            sleep.delay_ms(500u32);
+            blue_led.off();
+            sleep.delay_ms(500u32);
+        }
     }
 
+    // otherwise we jump to user code as usual
     const FINAL_ADDRESS: usize = 0x20010000;
     let user_main: fn() -> ! = unsafe { mem::transmute(FINAL_ADDRESS) };
     user_main();
